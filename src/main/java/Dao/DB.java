5503231499064d6a0474a909a26ca997201c6ddd
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-07-25 17:11:45 +0300

    change password credentials to access the database

--- src/main/java/Dao/DB.java
+++ src/main/java/Dao/DB.java
@@ -3,5 +3,5 @@ package Dao;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_projects", "muvaka", "Martins953");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_projects", "muvaka", "Access");
 }
