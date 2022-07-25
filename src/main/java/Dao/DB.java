8437ec9df53599b3397cd53d2c2afd5a2f6e90c3
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-07-25 12:32:04 +0300

    create an Sql2o variable to make a connection to the database

--- src/main/java/Dao/DB.java
+++ src/main/java/Dao/DB.java
@@ -1,2 +1,7 @@
-package Dao;public class DB {
+package Dao;
+
+import org.sql2o.Sql2o;
+
+public class DB {
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_projects", "muvaka", "Martins953");
 }
