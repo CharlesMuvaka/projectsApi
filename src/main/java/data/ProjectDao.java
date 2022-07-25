c193be9fe02b3471a6da46c080d8d0af0ee32216
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-07-25 12:37:19 +0300

    define a query to add a project to the database

--- src/main/java/data/ProjectDao.java
+++ src/main/java/data/ProjectDao.java
@@ -1,4 +1,46 @@
 package data;
 
-public class ProjectDao {
+import Dao.DB;
+import Dao.ProjectInterface;
+import models.Project;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class ProjectDao implements ProjectInterface {
+
+    @Override
+    public void addProject(Project project) {
+       String query = "INSERT INTO projects(name, type, url, link) VALUES(:name, :type, :url, :link)";
+       try(Connection conn = DB.sql2o.open()){
+           int id = (int) conn.createQuery(query, true)
+                   .addParameter("name", project.getName() )
+                   .addParameter("type", project.getType() )
+                   .addParameter("url", project.getUrl() )
+                   .addParameter("link", project.getLink() )
+                   .executeUpdate()
+                   .getKey();
+           project.setId(id);
+       }
+    }
+
+    @Override
+    public Project getProjectById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjectByType(String type) {
+        return null;
+    }
+
+    @Override
+    public void updateProject(int id, Project project) {
+
+    }
+
+    @Override
+    public void deleteProject(int id) {
+
+    }
 }
