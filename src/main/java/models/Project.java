0b0f7ff81a14dfac5f840d40ca9563f4a84053c2
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-07-25 11:54:26 +0300

    create a project construstor blueprint and member fields to define a single project instance

---
+++ src/main/java/models/Project.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Project{
+
+    private int id;
+    private String name, type, url, link;
+
+    public Project(String name, String type, String url, String link) {
+        this.name = name;
+        this.type = type;
+        this.url = url;
+        this.link = link;
+    }
+
+
+}
