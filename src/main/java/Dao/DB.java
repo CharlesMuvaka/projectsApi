a7f27c2011620d45eeac0af60bceda128bf2dcc0
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-07-27 03:19:44 +0300

    add heroku postgres database credentials

--- src/main/java/Dao/DB.java
+++ src/main/java/Dao/DB.java
@@ -3,5 +3,6 @@ package Dao;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_projects", "muvaka", "Access");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_projects", "muvaka", "Access");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-204-157-26.compute-1.amazonaws.com:5432/d65a5g3823d20p", "vvqrofszrkmjft", "69510a0ab543571942f59dd76b5927d5e7e55bf54aebce93c5aad388308e332a");
 }
